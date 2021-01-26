2b7b57313aebdb118c902db7b5558dec338a1563
Shivam Mishra <shimish2@cisco.com>
2021-01-26 14:26:18 -0800

    conformance: fix http status code for cross-repository mounting

--- pkg/api/routes.go
+++ pkg/api/routes.go
@@ -626,10 +626,23 @@ func (rh *RouteHandler) CreateBlobUpload(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	// blob mounts not allowed since we don't have access control yet, and this
-	// may be a uncommon use case, but remain compliant
-	if _, ok := r.URL.Query()["mount"]; ok {
-		w.WriteHeader(http.StatusMethodNotAllowed)
+	// currently zot does not support cross-repository mounting, following dist-spec and returning 202
+	if mountDigests, ok := r.URL.Query()["mount"]; ok {
+		if len(mountDigests) != 1 {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		u, err := rh.c.ImageStore.NewBlobUpload(name)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Location", path.Join(r.URL.String(), u))
+		w.Header().Set(BlobUploadUUID, u)
+		w.WriteHeader(http.StatusAccepted)
+
 		return
 	}
 
@@ -908,7 +921,6 @@ func (rh *RouteHandler) UpdateBlobUpload(w http.ResponseWriter, r *http.Request)
 	contentPresent := true
 
 	contentLen, err := strconv.ParseInt(r.Header.Get("Content-Length"), 10, 64)
-
 	if err != nil {
 		contentPresent = false
 	}
@@ -1070,7 +1082,6 @@ func getContentRange(r *http.Request) (int64 /* from */, int64 /* to */, error) 
 	tokens := strings.Split(contentRange, "-")
 
 	from, err := strconv.ParseInt(tokens[0], 10, 64)
-
 	if err != nil {
 		return -1, -1, errors.ErrBadUploadRange
 	}
@@ -1089,8 +1100,8 @@ func getContentRange(r *http.Request) (int64 /* from */, int64 /* to */, error) 
 
 func WriteJSON(w http.ResponseWriter, status int, data interface{}) {
 	var json = jsoniter.ConfigCompatibleWithStandardLibrary
-	body, err := json.Marshal(data)
 
+	body, err := json.Marshal(data)
 	if err != nil {
 		panic(err)
 	}
