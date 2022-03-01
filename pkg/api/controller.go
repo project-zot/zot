42db58790b342da0e1c55d5f6ad1e5c0edf532be
Ramkumar Chinchani <rchincha@cisco.com>
2022-03-01 20:57:56 +0000

    tls: restrict tls cipher suites
    golang will not turn them off by default:
    https://github.com/golang/go/issues/13385
    
    Signed-off-by: Ramkumar Chinchani <rchincha@cisco.com>

--- pkg/api/controller.go
+++ pkg/api/controller.go
@@ -182,8 +182,16 @@ func (c *Controller) Run() error {
 			}
 
 			server.TLSConfig = &tls.Config{
-				ClientAuth:               clientAuth,
-				ClientCAs:                caCertPool,
+				ClientAuth: clientAuth,
+				ClientCAs:  caCertPool,
+				CipherSuites: []uint16{
+					tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				},
 				PreferServerCipherSuites: true,
 				MinVersion:               tls.VersionTLS12,
 			}
