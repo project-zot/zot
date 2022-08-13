4b8e288cd3e66aa81c0f3c5c3399a162d964f01c
Ramkumar Chinchani <rchincha@cisco.com>
2022-08-13 03:53:23 +0000

    fix dependabot alerts
    https://github.com/project-zot/zot/pull/712
    https://github.com/project-zot/zot/pull/714
    
    Signed-off-by: Ramkumar Chinchani <rchincha@cisco.com>

--- pkg/api/controller_test.go
+++ pkg/api/controller_test.go
@@ -3629,7 +3629,7 @@ func TestImageSignatures(t *testing.T) {
 				options.RegistryOptions{AllowInsecure: true},
 				map[string]interface{}{"tag": "1.0"},
 				[]string{fmt.Sprintf("localhost:%s/%s@%s", port, repoName, digest.String())},
-				"", "", true, "", "", "", false, false, "")
+				"", "", true, "", "", "", false, false, "", true)
 			So(err, ShouldBeNil)
 
 			// verify the image
