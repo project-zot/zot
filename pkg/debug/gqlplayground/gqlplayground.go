c733b859a8b5791b35c02ed0b83d65c6108f6e6b
Catalin Hofnar <catalin.hofnar@gmail.com>
2022-08-26 17:02:23 +0300

    feat(GraphQL): playground, served by zot in specific binary
    Signed-off-by: Catalin Hofnar <catalin.hofnar@gmail.com>

---
+++ pkg/debug/gqlplayground/gqlplayground.go
@@ -0,0 +1,53 @@
+//go:build debug
+// +build debug
+
+package debug
+
+import (
+	"embed"
+	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"zotregistry.io/zot/pkg/api/config"
+	"zotregistry.io/zot/pkg/api/constants"
+	debugCst "zotregistry.io/zot/pkg/debug/constants"
+	"zotregistry.io/zot/pkg/log"
+	"zotregistry.io/zot/pkg/storage"
+)
+
+//go:embed index.html.tmpl
+var playgroundHTML embed.FS
+
+// SetupGQLPlaygroundRoutes ...
+func SetupGQLPlaygroundRoutes(conf *config.Config, router *mux.Router,
+	storeController storage.StoreController, l log.Logger,
+) {
+	log := log.Logger{Logger: l.With().Caller().Timestamp().Logger()}
+	log.Info().Msg("setting up graphql playground route")
+
+	templ, err := template.ParseFS(playgroundHTML, "gqlplayground/index.html.tmpl")
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+
+	//nolint:lll
+	router.PathPrefix(constants.RoutePrefix + debugCst.GQLPlaygroundEndpoint).HandlerFunc(func(writer http.ResponseWriter, req *http.Request) {
+		writer.Header().Add("Content-Type", "text/html")
+
+		proto := ""
+
+		if req.TLS == nil {
+			proto += "http://"
+		} else {
+			proto += "https://"
+		}
+
+		target := proto + req.Host + constants.ExtSearchPrefix
+
+		// respond with the output of template execution
+		_ = templ.Execute(writer, struct {
+			Target string
+		}{Target: target})
+	})
+}
---
+++ pkg/debug/gqlplayground/gqlplayground_disabled.go
@@ -0,0 +1,19 @@
+//go:build !debug
+// +build !debug
+
+package debug
+
+import (
+	"github.com/gorilla/mux"
+	"zotregistry.io/zot/pkg/api/config"
+	"zotregistry.io/zot/pkg/log"
+	"zotregistry.io/zot/pkg/storage"
+)
+
+// SetupGQLPlaygroundRoutes ...
+func SetupGQLPlaygroundRoutes(conf *config.Config, router *mux.Router,
+	storeController storage.StoreController, log log.Logger,
+) {
+	log.Warn().Msg("skipping enabling graphql playground extension because given zot binary" +
+		"doesn't include this feature, please build a binary that does so")
+}
