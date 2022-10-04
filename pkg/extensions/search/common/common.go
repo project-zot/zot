cbf8b2f3c5a8ad8b99867fa6bb179bbcda4692e6
Alex Stan <alexandrustan96@yahoo.ro>
2022-10-04 11:24:04 +0300

    feat(logos): Support storing image logos as manifest annotations (#833)
    This commit allows storing base64 encoded logos under image manifest annotations, using com.zot.logo key.
    Logos have type limitations (only jpeg, png and gif allowed) and a maximum allowed size (200x200 px).
    
    Signed-off-by: Alex Stan <alexandrustan96@yahoo.ro>

--- pkg/extensions/search/common/common.go
+++ pkg/extensions/search/common/common.go
@@ -23,6 +23,7 @@ const (
 	LabelAnnotationTitle         = "org.label-schema.name"
 	LabelAnnotationDocumentation = "org.label-schema.usage"
 	LabelAnnotationSource        = "org.label-schema.vcs-url"
+	logoKey                      = "com.zot.logo"
 )
 
 type TagInfo struct {
@@ -169,6 +170,7 @@ type ImageAnnotations struct {
 	Source        string
 	Labels        string
 	Vendor        string
+	Logo          string
 }
 
 /*
@@ -219,6 +221,12 @@ func GetCategories(labels map[string]string) string {
 	return categories
 }
 
+func GetLogo(annotations map[string]string) string {
+	logo := annotations[logoKey]
+
+	return logo
+}
+
 func GetAnnotations(annotations, labels map[string]string) ImageAnnotations {
 	description := GetDescription(annotations)
 	if description == "" {
@@ -255,6 +263,11 @@ func GetAnnotations(annotations, labels map[string]string) ImageAnnotations {
 		vendor = GetVendor(labels)
 	}
 
+	logo := GetLogo(annotations)
+	if logo == "" {
+		logo = GetLogo(labels)
+	}
+
 	return ImageAnnotations{
 		Description:   description,
 		Title:         title,
@@ -263,5 +276,6 @@ func GetAnnotations(annotations, labels map[string]string) ImageAnnotations {
 		Licenses:      licenses,
 		Labels:        categories,
 		Vendor:        vendor,
+		Logo:          logo,
 	}
 }
