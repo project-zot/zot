ec05137edac8c2e46c1b7f487592f5d79286fb31
Andreea Lupu <58118008+Andreea-Lupu@users.noreply.github.com>
2022-12-11 02:38:01 +0200

    feat(scrub): add scrub logic for ImageIndex media type (#1031)
    Signed-off-by: Andreea-Lupu <andreealupu1470@yahoo.com>

--- pkg/storage/scrub.go
+++ pkg/storage/scrub.go
@@ -115,67 +115,103 @@ func CheckRepo(imageName string, imgStore ImageStore) ([]ScrubImageResult, error
 		return results, errors.ErrRepoNotFound
 	}
 
-	for _, m := range index.Manifests {
-		tag, ok := m.Annotations[ispec.AnnotationRefName]
-		if ok {
-			imageResult := checkIntegrity(ctxUmoci, imageName, tag, oci, m, dir)
-			results = append(results, imageResult)
+	listOfManifests := []ispec.Descriptor{}
+
+	for _, manifest := range index.Manifests {
+		if manifest.MediaType == ispec.MediaTypeImageIndex {
+			buf, err := os.ReadFile(path.Join(dir, "blobs", manifest.Digest.Algorithm().String(), manifest.Digest.Encoded()))
+			if err != nil {
+				tagName := manifest.Annotations[ispec.AnnotationRefName]
+				imgRes := getResult(imageName, tagName, errors.ErrBadBlobDigest)
+				results = append(results, imgRes)
+
+				continue
+			}
+
+			var idx ispec.Index
+			if err := json.Unmarshal(buf, &idx); err != nil {
+				tagName := manifest.Annotations[ispec.AnnotationRefName]
+				imgRes := getResult(imageName, tagName, errors.ErrBadBlobDigest)
+				results = append(results, imgRes)
+
+				continue
+			}
+
+			listOfManifests = append(listOfManifests, idx.Manifests...)
+		} else if manifest.MediaType == ispec.MediaTypeImageManifest {
+			listOfManifests = append(listOfManifests, manifest)
 		}
+	}
+
+	for _, m := range listOfManifests {
+		tag := m.Annotations[ispec.AnnotationRefName]
+		imageResult := CheckIntegrity(ctxUmoci, imageName, tag, oci, m, dir)
+		results = append(results, imageResult)
 	}
 
 	return results, nil
 }
 
-func checkIntegrity(ctx context.Context, imageName, tagName string, oci casext.Engine, manifest ispec.Descriptor, dir string) ScrubImageResult { //nolint: lll
+func CheckIntegrity(ctx context.Context, imageName, tagName string, oci casext.Engine, manifest ispec.Descriptor, dir string) ScrubImageResult { //nolint: lll
 	// check manifest and config
-	stat, err := umoci.Stat(ctx, oci, manifest)
+	if _, err := umoci.Stat(ctx, oci, manifest); err != nil {
+		return getResult(imageName, tagName, err)
+	}
 
+	// check layers
+	return CheckLayers(imageName, tagName, dir, manifest)
+}
+
+func CheckLayers(imageName, tagName, dir string, manifest ispec.Descriptor) ScrubImageResult {
 	imageRes := ScrubImageResult{}
 
+	buf, err := os.ReadFile(path.Join(dir, "blobs", manifest.Digest.Algorithm().String(), manifest.Digest.Encoded()))
 	if err != nil {
 		imageRes = getResult(imageName, tagName, err)
-	} else {
-		// check layers
-		for _, s := range stat.History {
-			layer := s.Layer
-			if layer == nil {
-				continue
-			}
 
-			// check layer
-			layerPath := path.Join(dir, "blobs", layer.Digest.Algorithm().String(), layer.Digest.Encoded())
+		return imageRes
+	}
 
-			_, err = os.Stat(layerPath)
-			if err != nil {
-				imageRes = getResult(imageName, tagName, errors.ErrBlobNotFound)
+	var man ispec.Manifest
+	if err := json.Unmarshal(buf, &man); err != nil {
+		imageRes = getResult(imageName, tagName, err)
 
-				break
-			}
+		return imageRes
+	}
 
-			layerFh, err := os.Open(layerPath)
-			if err != nil {
-				imageRes = getResult(imageName, tagName, errors.ErrBlobNotFound)
+	for _, layer := range man.Layers {
+		layerPath := path.Join(dir, "blobs", layer.Digest.Algorithm().String(), layer.Digest.Encoded())
 
-				break
-			}
+		_, err = os.Stat(layerPath)
+		if err != nil {
+			imageRes = getResult(imageName, tagName, errors.ErrBlobNotFound)
 
-			computedDigest, err := godigest.FromReader(layerFh)
-			layerFh.Close()
-
-			if err != nil {
-				imageRes = getResult(imageName, tagName, errors.ErrBadBlobDigest)
-
-				break
-			}
-
-			if computedDigest != layer.Digest {
-				imageRes = getResult(imageName, tagName, errors.ErrBadBlobDigest)
-
-				break
-			}
-
-			imageRes = getResult(imageName, tagName, nil)
+			break
 		}
+
+		layerFh, err := os.Open(layerPath)
+		if err != nil {
+			imageRes = getResult(imageName, tagName, errors.ErrBlobNotFound)
+
+			break
+		}
+
+		computedDigest, err := godigest.FromReader(layerFh)
+		layerFh.Close()
+
+		if err != nil {
+			imageRes = getResult(imageName, tagName, errors.ErrBadBlobDigest)
+
+			break
+		}
+
+		if computedDigest != layer.Digest {
+			imageRes = getResult(imageName, tagName, errors.ErrBadBlobDigest)
+
+			break
+		}
+
+		imageRes = getResult(imageName, tagName, nil)
 	}
 
 	return imageRes
