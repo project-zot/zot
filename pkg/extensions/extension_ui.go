fff1b9a32fbf86333bd50c0dbe3cdb51c1916783
Andrei Aaron <aaaron@luxoft.com>
2023-04-04 14:08:37 +0000

    fix(zap): add Content-Security-Policy to the UI routes
    The zap scanner started to check the csp header, which is causing a warning.
    
    We also need to ignore the rule, as both settings are read by the scanner.
    
    Per https://w3c.github.io/webappsec-csp/#example-7bb4ce67 we can have multiple
    Content-Security-Policy headers, and the most restrictive policies apply.
    This rule doesn't seem to be applied by zap.
    
    * URL: http://localhost:8080/
      * Method: `GET`
      * Parameter: `content-security-policy`
      * Attack: ``
      * Evidence: ` default-src 'none'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com/; font-src 'self' https://fonts.googleapis.com/ https://fonts.gstatic.com/; connect-src *; img-src 'self'; manifest-src 'self'; base-uri 'self'`
    * URL: http://localhost:8080/
      * Method: `GET`
      * Parameter: `Content-Security-Policy`
      * Attack: ``
      * Evidence: `default-src 'none'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com/; font-src 'self' https://fonts.googleapis.com/ https://fonts.gstatic.com/; connect-src 'self'; img-src 'self'; manifest-src 'self'; base-uri 'self'`
    
    Signed-off-by: Andrei Aaron <aaaron@luxoft.com>

--- pkg/extensions/extension_ui.go
+++ pkg/extensions/extension_ui.go
@@ -7,6 +7,7 @@ import (
 	"embed"
 	"io/fs"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 
@@ -39,6 +40,18 @@ func addUISecurityHeaders(h http.Handler) http.HandlerFunc { //nolint:varnamelen
 		w.Header().Set("Permissions-Policy", permissionsPolicy)
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "DENY")
+
+		cspDirectives := []string{
+			"default-src 'none'",
+			"script-src 'self' 'unsafe-inline'",
+			"style-src 'self' 'unsafe-inline' https://fonts.googleapis.com/",
+			"font-src 'self' https://fonts.googleapis.com/ https://fonts.gstatic.com/",
+			"connect-src 'self'",
+			"img-src 'self'",
+			"manifest-src 'self'",
+			"base-uri 'self'",
+		}
+		w.Header().Set("Content-Security-Policy", strings.Join(cspDirectives, "; "))
 
 		h.ServeHTTP(w, r)
 	}
