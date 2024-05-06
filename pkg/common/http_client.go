be5ad667974b43905a118a40435f7117c0dde511
Vishwas R <30438425+vrajashkr@users.noreply.github.com>
2024-05-07 02:13:41 +0530

    refactor(http): refactor http client to accept more customisable options (#2414)
    refactor(http): refactor http client to take options struct
    
    This commit updates the arguments for the `CreateHTTPClient`
    function to consume a struct which can be extended as required.
    It replaces the certPath argument with a struct of 3 paths for
    client ertificate, client key, and ca cert. It also adds
    a TLSEnabled option for when an HTTP Client is required
    without any further TLS config.
    
    Existing consumers of this function have been updated so that
    they can work as they do today. This change is a no-op for
    existing features.
    
    This allows for certificate paths to be customised and
    allows other modules to re-use the same HTTP client and get
    the benefits of mTLS support and per-host certificates.
    
    Signed-off-by: Vishwas Rajashekar <vrajashe@cisco.com>

--- pkg/common/http_client.go
+++ pkg/common/http_client.go
@@ -5,14 +5,13 @@ import (
 	"crypto/x509"
 	"net/http"
 	"os"
-	"path"
 	"path/filepath"
 )
 
 func GetTLSConfig(certsPath string, caCertPool *x509.CertPool) (*tls.Config, error) {
-	clientCert := filepath.Join(certsPath, clientCertFilename)
-	clientKey := filepath.Join(certsPath, clientKeyFilename)
-	caCertFile := filepath.Join(certsPath, caCertFilename)
+	clientCert := filepath.Join(certsPath, ClientCertFilename)
+	clientKey := filepath.Join(certsPath, ClientKeyFilename)
+	caCertFile := filepath.Join(certsPath, CaCertFilename)
 
 	cert, err := tls.LoadX509KeyPair(clientCert, clientKey)
 	if err != nil {
@@ -59,9 +58,43 @@ func loadPerHostCerts(caCertPool *x509.CertPool, host string) *tls.Config {
 	return nil
 }
 
-func CreateHTTPClient(verifyTLS bool, host string, certDir string) (*http.Client, error) {
+// Holds certificate options for an HTTP client.
+type HTTPClientCertOptions struct {
+	ClientCertFile string // Holds the path to the client certificate file. Mandatory if ClientKeyFile is present.
+	ClientKeyFile  string // Holds the path to the client key file. Mandatory if ClientCertFile is present.
+	RootCaCertFile string // Optional. Holds the path to the custom Root CA cert file.
+}
+
+// Holds client options for creating an HTTP client.
+type HTTPClientOptions struct {
+	// Results in a client with TLS config if true.
+	TLSEnabled bool
+
+	// Results in a client without certificate config and TLS verification disabled if true.
+	// Note: if TLSEnabled is false and VerifyTLS is true, the client will not have the verification
+	// of insecure certificates set to false. For this, both TLSEnabled and VerifyTLS need to be
+	// true.
+	VerifyTLS bool
+
+	// The target host for the imminent connection. Used for loading host specific certificates if any.
+	Host string
+
+	// Certificate options for the client.
+	CertOptions HTTPClientCertOptions
+}
+
+func CreateHTTPClient(clientOptions *HTTPClientOptions) (*http.Client, error) {
 	htr := http.DefaultTransport.(*http.Transport).Clone() //nolint: forcetypeassert
-	if !verifyTLS {
+
+	// If TLS is not enabled, return the client without any further TLS config.
+	if !clientOptions.TLSEnabled {
+		return &http.Client{
+			Timeout:   httpTimeout,
+			Transport: htr,
+		}, nil
+	}
+
+	if !clientOptions.VerifyTLS {
 		htr.TLSClientConfig = &tls.Config{InsecureSkipVerify: true} //nolint: gosec
 
 		return &http.Client{
@@ -70,35 +103,36 @@ func CreateHTTPClient(verifyTLS bool, host string, certDir string) (*http.Client
 		}, nil
 	}
 
-	// Add a copy of the system cert pool
+	// Add a copy of the system cert pool.
 	caCertPool, _ := x509.SystemCertPool()
 
-	tlsConfig := loadPerHostCerts(caCertPool, host)
-	if tlsConfig == nil {
-		tlsConfig = &tls.Config{RootCAs: caCertPool, MinVersion: tls.VersionTLS12}
-	}
-
-	htr.TLSClientConfig = tlsConfig
-
-	if certDir != "" {
-		clientCert := path.Join(certDir, "client.cert")
-		clientKey := path.Join(certDir, "client.key")
-		caCertPath := path.Join(certDir, "ca.crt")
-
-		caCert, err := os.ReadFile(caCertPath)
+	// Add a custom CA cert if present in the options.
+	if clientOptions.CertOptions.RootCaCertFile != "" {
+		caCert, err := os.ReadFile(clientOptions.CertOptions.RootCaCertFile)
 		if err != nil {
 			return nil, err
 		}
 
 		caCertPool.AppendCertsFromPEM(caCert)
+	}
 
-		cert, err := tls.LoadX509KeyPair(clientCert, clientKey)
+	// Load certificates specific to the host if any.
+	tlsConfig := loadPerHostCerts(caCertPool, clientOptions.Host)
+	if tlsConfig == nil {
+		tlsConfig = &tls.Config{RootCAs: caCertPool, MinVersion: tls.VersionTLS12}
+	}
+
+	// Try to load certificate key pair if either are present in the options.
+	if clientOptions.CertOptions.ClientCertFile != "" || clientOptions.CertOptions.ClientKeyFile != "" {
+		cert, err := tls.LoadX509KeyPair(clientOptions.CertOptions.ClientCertFile, clientOptions.CertOptions.ClientKeyFile)
 		if err != nil {
 			return nil, err
 		}
 
-		htr.TLSClientConfig.Certificates = append(htr.TLSClientConfig.Certificates, cert)
+		tlsConfig.Certificates = append(tlsConfig.Certificates, cert)
 	}
+
+	htr.TLSClientConfig = tlsConfig
 
 	return &http.Client{
 		Transport: htr,
