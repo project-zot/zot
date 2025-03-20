affc10ded7dd00061d0f7e89979c57c46af12408
Piaras Hoban <phoban01@gmail.com>
2025-03-20 16:10:01 +0000

    feat: implement http and nats sinks. remove log sink
    Signed-off-by: Piaras <phoban01@gmail.com>
    Signed-off-by: Piaras Hoban <phoban01@gmail.com>

--- pkg/extensions/events/events_test.go
+++ pkg/extensions/events/events_test.go
@@ -4,13 +4,20 @@
 package events_test
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 
 	cloudevents "github.com/cloudevents/sdk-go/v2"
+	cehttp "github.com/cloudevents/sdk-go/v2/protocol/http"
 	"github.com/google/go-containerregistry/pkg/v1/types"
+	"github.com/nats-io/nats-server/v2/server"
+	"github.com/nats-io/nats.go"
 	. "github.com/smartystreets/goconvey/convey"
 
 	zerr "zotregistry.dev/zot/errors"
+	eventsconf "zotregistry.dev/zot/pkg/extensions/config/events"
 	"zotregistry.dev/zot/pkg/extensions/events"
 	"zotregistry.dev/zot/pkg/log"
 )
@@ -25,6 +32,10 @@ func (s *mockSink) Emit(e *cloudevents.Event) cloudevents.Result {
 	return nil
 }
 
+func (s *mockSink) Close() error {
+	return nil
+}
+
 var _ events.Sink = (*mockSink)(nil)
 
 func newMockSink() *mockSink {
@@ -35,7 +46,7 @@ func newMockSink() *mockSink {
 
 func TestEventSinkMissing(t *testing.T) {
 	Convey("missing sink", t, func() {
-		_, err := events.NewRecorder(nil, log.NewLogger("debug", ""))
+		_, err := events.NewRecorder(log.NewLogger("debug", ""))
 		So(err, ShouldNotBeNil)
 		So(err, ShouldEqual, zerr.ErrEventSinkIsNil)
 	})
@@ -44,7 +55,7 @@ func TestEventSinkMissing(t *testing.T) {
 func TestEvents(t *testing.T) {
 	Convey("emits events", t, func() {
 		sink := newMockSink()
-		recorder, err := events.NewRecorder(sink, log.NewLogger("debug", ""))
+		recorder, err := events.NewRecorder(log.NewLogger("debug", ""), sink)
 		So(err, ShouldBeNil)
 		Convey("repository created", func() {
 			err = recorder.RepositoryCreated("test")
@@ -67,4 +78,177 @@ func TestEvents(t *testing.T) {
 			So(sink.store[events.ImageLintFailedEventType.String()], ShouldEqual, 1)
 		})
 	})
+}
+
+func TestHTTPSink(t *testing.T) {
+	Convey("emits events to http sink", t, func() {
+		var receivedEvent *cloudevents.Event
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			event, err := cehttp.NewEventFromHTTPRequest(r)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+
+				return
+			}
+			receivedEvent = event
+
+			w.WriteHeader(http.StatusOK)
+		}))
+
+		defer server.Close()
+
+		config := eventsconf.SinkConfig{
+			Type:    eventsconf.HTTP,
+			Address: server.URL,
+			Timeout: 5 * time.Second,
+		}
+		sink, err := events.NewHTTPSink(config)
+		So(err, ShouldBeNil)
+
+		recorder, err := events.NewRecorder(log.NewLogger("debug", ""), sink)
+		So(err, ShouldBeNil)
+
+		Convey("repository created", func() {
+			err = recorder.RepositoryCreated("test")
+			So(err, ShouldBeNil)
+			So(receivedEvent, ShouldNotBeNil)
+			So(receivedEvent.Type(), ShouldEqual, events.RepositoryCreatedEventType.String())
+		})
+
+		Convey("image updated", func() {
+			err = recorder.ImageUpdated("test", "v1", "", string(types.OCIManifestSchema1), "")
+			So(err, ShouldBeNil)
+			So(receivedEvent, ShouldNotBeNil)
+			So(receivedEvent.Type(), ShouldEqual, events.ImageUpdatedEventType.String())
+		})
+
+		Convey("image deleted", func() {
+			err = recorder.ImageDeleted("test", "v1", "", string(types.OCIManifestSchema1))
+			So(err, ShouldBeNil)
+			So(receivedEvent, ShouldNotBeNil)
+			So(receivedEvent.Type(), ShouldEqual, events.ImageDeletedEventType.String())
+		})
+
+		Convey("image lint failed", func() {
+			err = recorder.ImageLintFailed("test", "v1", "", string(types.OCIManifestSchema1), "")
+
+			So(receivedEvent, ShouldNotBeNil)
+			So(receivedEvent.Type(), ShouldEqual, events.ImageLintFailedEventType.String())
+		})
+	})
+}
+
+func TestNATSSink(t *testing.T) {
+	Convey("emits events to nats sink", t, func() {
+		natsServer, natsURL := setupTestNATSServer()
+		defer natsServer.Shutdown()
+
+		var receivedEvent *cloudevents.Event
+		testChannel := "test-events"
+
+		nc, err := nats.Connect(natsURL)
+		So(err, ShouldBeNil)
+		defer nc.Close()
+
+		_, err = nc.Subscribe(testChannel, func(msg *nats.Msg) {
+			event := cloudevents.NewEvent()
+
+			headers := msg.Header
+			event.SetID(headers.Get("ce-id"))
+			event.SetSource(headers.Get("ce-source"))
+			event.SetType(headers.Get("ce-type"))
+
+			if subj := headers.Get("ce-subject"); subj != "" {
+				event.SetSubject(subj)
+			}
+
+			if err := event.UnmarshalJSON(msg.Data); err == nil {
+				receivedEvent = &event
+			}
+
+			_ = msg.Respond([]byte("OK"))
+		})
+		So(err, ShouldBeNil)
+
+		config := eventsconf.SinkConfig{
+			Type:    eventsconf.NATS,
+			Address: natsURL,
+			Channel: testChannel,
+			Timeout: 5 * time.Second,
+		}
+
+		sink, err := events.NewNATSSink(config)
+		So(err, ShouldBeNil)
+		defer sink.Close()
+
+		recorder, err := events.NewRecorder(log.NewLogger("debug", ""), sink)
+		So(err, ShouldBeNil)
+
+		Convey("repository created", func() {
+			receivedEvent = nil
+			err = recorder.RepositoryCreated("test")
+
+			time.Sleep(100 * time.Millisecond)
+
+			So(err, ShouldBeNil)
+			So(receivedEvent, ShouldNotBeNil)
+			So(receivedEvent.Type(), ShouldEqual, events.RepositoryCreatedEventType.String())
+		})
+
+		Convey("image updated", func() {
+			receivedEvent = nil
+			err = recorder.ImageUpdated("test", "v1", "", string(types.OCIManifestSchema1), "")
+
+			time.Sleep(100 * time.Millisecond)
+
+			So(err, ShouldBeNil)
+			So(receivedEvent, ShouldNotBeNil)
+			So(receivedEvent.Type(), ShouldEqual, events.ImageUpdatedEventType.String())
+		})
+
+		Convey("image deleted", func() {
+			receivedEvent = nil
+			err = recorder.ImageDeleted("test", "v1", "", string(types.OCIManifestSchema1))
+
+			time.Sleep(100 * time.Millisecond)
+
+			So(err, ShouldBeNil)
+			So(receivedEvent, ShouldNotBeNil)
+			So(receivedEvent.Type(), ShouldEqual, events.ImageDeletedEventType.String())
+		})
+
+		Convey("image lint failed", func() {
+			receivedEvent = nil
+			err = recorder.ImageLintFailed("test", "v1", "", string(types.OCIManifestSchema1), "")
+
+			time.Sleep(100 * time.Millisecond)
+
+			So(err, ShouldBeNil)
+			So(receivedEvent, ShouldNotBeNil)
+			So(receivedEvent.Type(), ShouldEqual, events.ImageLintFailedEventType.String())
+		})
+	})
+}
+
+func setupTestNATSServer() (*server.Server, string) {
+	opts := server.Options{
+		Host:           "127.0.0.1",
+		Port:           -1, // Use random available port
+		NoLog:          true,
+		NoSigs:         true,
+		MaxControlLine: 4096,
+	}
+
+	natsServer, err := server.NewServer(&opts)
+	if err != nil {
+		panic(err)
+	}
+
+	go natsServer.Start()
+
+	if !natsServer.ReadyForConnections(5 * time.Second) {
+		panic("NATS server failed to start")
+	}
+
+	return natsServer, natsServer.ClientURL()
 }
