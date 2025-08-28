f0d1c81f59b0159dc55dd2561fc97ebcb8f06e4f
Vishwas Rajashekar <30438425+vrajashkr@users.noreply.github.com>
2025-08-28 20:11:01 +0530

    feat(sessions): add support for remote redis session store
    Description
    ====================
    zot currently stores session cookies in memory or in a local directory.
    For cases where the session cookies should be independent of the
    instance where they were created such as multiple instances of zot, or a
    fully stateless zot instance, there is a need to support a remote
    session storage.
    This PR adds support for using Redis and Redis-compatible services as a
    remote session driver as well as introduces a new configuration option
    for it.
    
    What has changed
    =======================
    - New config added under UI Extension config to specify configuration for
      the session driver.
    - The config supports only 2 drivers in this PR - local and redis
    - Using the local driver is backwards compatible and behaves the same
      way that zot currently works for local session storage.
    - Omitting this config does not result in an error. In this case, zot
      behaves as it normally does for local session storage.
    - When configured, zot can use redis for persisting cookie
      information for zot UI.
    - The cookie in the store is deleted on logout or after the max
      expiry time for the cookie.
    - Configuration for the redis session driver accepts the same configuration
      values as that of the remote meta cache.
    - A separate connection is established for the session driver. An
      existing connection for meta cache will not be re-used for the
      session driver.
    - A key prefix is configurable for the redis session driver. The value will be
      converted into a string for use. If no value is provided, a default
      prefix of "zotsession" will be used.
    
    Signed-off-by: Vishwas Rajashekar <30438425+vrajashkr@users.noreply.github.com>

--- pkg/cli/server/root.go
+++ pkg/cli/server/root.go
@@ -325,6 +325,46 @@ func validateCacheConfig(cfg *config.Config, log zlog.Logger) error {
 	return nil
 }
 
+func validateRemoteSessionStoreConfig(cfg *config.Config, log zlog.Logger) error {
+	// it is okay for the session driver config to be nil
+	// this is backwards compatible for older configs
+	if cfg.Extensions.UI.SessionDriver == nil {
+		return nil
+	}
+
+	sessionDriverName, ok := cfg.Extensions.UI.SessionDriver["name"]
+	if !ok {
+		msg := "must provide session driver name!"
+		log.Error().Err(zerr.ErrBadConfig).Msg(msg)
+
+		return fmt.Errorf("%w: %s", zerr.ErrBadConfig, msg)
+	}
+
+	allowedDriverNames := []string{
+		storageConstants.RedisDriverName,
+		storageConstants.LocalStorageDriverName,
+	}
+
+	isValidDriver := false
+
+	for _, allowedDriverName := range allowedDriverNames {
+		if allowedDriverName == sessionDriverName {
+			isValidDriver = true
+
+			break
+		}
+	}
+
+	if !isValidDriver {
+		msg := fmt.Sprintf("session store driver %s is not allowed!", sessionDriverName)
+		log.Error().Err(zerr.ErrBadConfig).Msg(msg)
+
+		return fmt.Errorf("%w: %s", zerr.ErrBadConfig, msg)
+	}
+
+	return nil
+}
+
 func validateExtensionsConfig(cfg *config.Config, log zlog.Logger) error {
 	if cfg.Extensions != nil && cfg.Extensions.Mgmt != nil {
 		log.Warn().Msg("mgmt extensions configuration option has been made redundant and will be ignored.")
@@ -343,6 +383,11 @@ func validateExtensionsConfig(cfg *config.Config, log zlog.Logger) error {
 			log.Error().Err(zerr.ErrBadConfig).Msg(msg)
 
 			return fmt.Errorf("%w: %s", zerr.ErrBadConfig, msg)
+		}
+
+		err := validateRemoteSessionStoreConfig(cfg, log)
+		if err != nil {
+			return err
 		}
 	}
 
