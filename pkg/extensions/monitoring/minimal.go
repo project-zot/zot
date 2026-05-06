2a9348d3d3ec62c4db94f81eccf5b4ad6291545c
Benoit Tigeot <benoit.tigeot@lifen.fr>
2026-05-06 09:49:26 +0200

    fix(monitoring): expose metric types outside build tag
    `MetricsCopy` and related types were only visible under `\!metrics`,
    causing a typecheck failure when golangci-lint runs with `-tags metrics`.
    Moving the type definitions to `common.go` makes them unconditionally available.
    
    Signed-off-by: Benoit Tigeot <benoit.tigeot@lifen.fr>

--- pkg/extensions/monitoring/minimal.go
+++ pkg/extensions/monitoring/minimal.go
@@ -56,55 +56,6 @@ type metricServer struct {
 	stopChan   chan struct{} // Channel to signal shutdown
 }
 
-type MetricsInfo struct {
-	Counters   []*CounterValue
-	Gauges     []*GaugeValue
-	Summaries  []*SummaryValue
-	Histograms []*HistogramValue
-}
-type MetricsCopy struct {
-	Counters   []CounterValue
-	Gauges     []GaugeValue
-	Summaries  []SummaryValue
-	Histograms []HistogramValue
-}
-
-// CounterValue stores info about a metric that is incremented over time,
-// such as the number of requests to an HTTP endpoint.
-type CounterValue struct {
-	Name        string
-	Count       int
-	LabelNames  []string
-	LabelValues []string
-}
-
-// GaugeValue stores one value that is updated as time goes on, such as
-// the amount of memory allocated.
-type GaugeValue struct {
-	Name        string
-	Value       float64
-	LabelNames  []string
-	LabelValues []string
-}
-
-// SummaryValue stores info about a metric that is incremented over time,
-// such as the number of requests to an HTTP endpoint.
-type SummaryValue struct {
-	Name        string
-	Count       int
-	Sum         float64
-	LabelNames  []string
-	LabelValues []string
-}
-
-type HistogramValue struct {
-	Name        string
-	Count       int
-	Sum         float64
-	Buckets     map[string]int
-	LabelNames  []string
-	LabelValues []string
-}
 
 func GetDefaultBuckets() []float64 {
 	return []float64{.05, .5, 1, 5, 30, 60, 600, math.MaxFloat64}
